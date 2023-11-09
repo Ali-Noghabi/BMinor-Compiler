701687870721cb146d852ce9d8c28263bdbdd173
Ghaf0o0r <150042871+Ghaf0o0r@users.noreply.github.com>
2023-11-09 00:43:23 -0800

    add and complete some functions

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ enum Type {
 };
 const char* keyWords[] = { "array", "boolean", "char", "else", "false", "for", "function", "if",
 								"integer", "print", "return", "string", "true", "void", "while"};
+int line = 1;
 const char operators[] = {'+' , '-' ,'!' ,'*' , '/' , '%' ,'=' ,'|' ,'^' , '<' , '>' , '&' };
 const char delimeters[]= {'(' , ')' , '[' , ']' , '{' , '}' , ',' , ';' , ':' , '\'' , '"' , '\\'};
 struct Token{
@@ -20,19 +21,80 @@ struct Token{
     char* value;
     int line ;
 };
-void recongnizer(char* word){
+int  isItValidInteger(char * word){
+    /// be smaller than  Max-64-bit integer
+    for(int i=0; i<strlen(word) ; i++){
+        if(isdigit(word[i])){
+            continue;
+        }
+        return 0;
+    }
+    return 1;
 }
-int isOperator(char c) {
+int isItValidID(char * word){
+    if(strlen(word) > 256 ){
+        /// error
+        return;
+    }
+    for(int i=1; i<strlen(word) ; i++){
+        if(isdigit(word[i]) || isalpha(word[i]) || word[i] == '_'){
+            continue;
+        }
+        return 0;
+    }
+    return 1;
+}
+void recongnizer(char* word){
+    if(isdigit(word[0])){
+        if(isItValidInteger(word)){
+            struct Token token ;
+            token.type = Integer;
+            token.line = line;
+            token.value = (char *) malloc(strlen(word) + 1);
+            strcpy(token.value , word);
+        }else{
+            /// error
+        }
+    }else{
+        if(isalpha(word[0]) || word[0] == '_'){
+            if(isItValidID(word)){
+                struct Token token ;
+                token.type = ID;
+                token.line = line;
+                token.value = (char *) malloc(strlen(word) + 1);
+                strcpy(token.value , word);
+
+            }else{
+                /// error
+            }
+        }
+        else{
+            /// error
+        }
+    }
 
 }
+int isOperator(char c) {
+    for(int i =0 ; i < sizeof(operators) / sizeof(char) ; i++){
+        if(c == operators[i]){
+            return 1;
+        }
+    }
+    return 0;
+}
 int isDelimeters(char c){
+    for(int i =0 ; i < sizeof(delimeters) / sizeof(char) ; i++){
+        if(c == delimeters[i]){
+            return 1;
+        }
+    }
+    return 0;
 }
 void stringReader(FILE* file){
 }
 void operatorReader(char c , FILE* file){
 }
 void lexical_analysis(FILE* file){
-    int line = 1;
     char c;
     char* str =NULL;
     while((c = fgetc(file)) != EOF){
